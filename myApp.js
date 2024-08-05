b6be815dc4a5ad2ab87cf8680002527547ca4b02
pryanik <tulendikozha@gmail.com>
2024-08-05 09:53:06 +0500

    Update README.md

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3030;
 
+app.listen(PORT, () => {
+  console.log(`Useful Programmer Into Security App started on port ${PORT}`);
+});
 
 
 
